4cc77f41a35f93131e902c52a6acd001bc795a18
Thiago <thiago3399@hotmail.com>
2021-02-24 20:44:06 -0300

    [27] Added routes and controller functions
    Co-authored-by: Lucas <lucascandradex14@gmail.com>

--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -1,5 +1,45 @@
-const index = (request, response) =>{ 
-    return response.json({user: 'user'})
+const accounts = []
+
+const signUpGet = (req, res) => {
+    return res.json(accounts)
 }
 
-module.exports = {index}
+const signUpPost = (req, res) => {
+    const {name, email, enroll, pass} = req.body;
+
+    const user = {
+        name: name,
+        email: email,
+        email: enroll,
+        pass: pass
+    }
+
+    accounts.push(user)
+
+    return res.json(accounts)
+}
+
+const signUpPut = (req, res) => {
+    const id = req.params.id
+
+    const {name, email, enroll, pass} = req.body;
+
+    accounts[id - 1].name = name
+    accounts[id - 1].email = email
+    accounts[id - 1].enroll = enroll
+    accounts[id - 1].pass = pass
+
+    return res.json(contas)
+}
+
+const signUpDelete = (req, res) => {
+    const id = req.params.id
+
+    if (id >= 1) {
+        contas.splice(id - 1, 1)
+    }
+
+    return res.json(contas);
+}
+
+module.exports = {signUpGet, signUpPost, signUpPut, signUpDelete}
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,13 @@ const routes = express.Router();
 
 const UserController = require('./Controllers/UserController')
 
-routes.get('/user', UserController.index) 
+//routes.get('/user', UserController.index) 
+routes.get('/index', UserController.signUpGet)
+routes.post('/signUp', UserController.signUpPost)
+routes.put('/update/:id', UserController.signUpPut)
+routes.delete('/delete/:id', UserController.signUpDelete)
+
 
 module.exports = routes;
+
+
