bb973e5b74c8e36c8da1bdb86615b3ad07299ba7
lucas <lucascandradex14@gmail.com>
2021-03-05 10:34:23 -0300

    [34] fix some validation issues
    Co-authored-by: Joao Pedro <joaopedroaschaves@gmail.com>

--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -1,118 +1,98 @@
-const User = require('../Models/UserSchema');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-require('dotenv-safe').config();
-
-
-const signUpGet = async (req, res, next) => {
-    const users = await User.find()
-    
-    return res.json(users);
-}
-
-const signUpPost = async (req, res) => {
-    let {name, email, enroll, pass} = req.body;
-
-    if (!validate(name, email, enroll, pass)) {
-        return res.json({"message":"invalid"});
-    }
-
-    const user = await User.create({
-        name: name, 
-        email: email,
-        enroll: enroll,
-        pass: await hashPass(pass)
-    });
-
-    return res.json(user);
-}
-
-const signUpPut = async (req, res) => {
-    const id = req.params.id;
-    let {name, email, enroll, pass} = req.body;
-
-    if (!validate(name, email, enroll, pass)) {
-        return res.json({"message":"invalid"});
-    }
-
-    const usuarioEncontrado = await User.findOne({_id:id});
-
-    //senha nao se altera
-    if (await bcrypt.compare(req.body.pass, usuarioEncontrado.pass)) {
-        pass = usuarioEncontrado.pass;
-    } 
-    //senha alterada
-    else { 
-        pass = await hashPass(pass);
-    }
-
-    const updateReturn = await User.findOneAndUpdate({_id:id}, {name, email, enroll, pass}, 
-        { new:  true }, (err, user)=>{
-            if (err) {
-                return res.json(err);
-            } else {
-                return res.json(user);
-            }
-    })
-}
-
-const signUpDelete = async (req, res) => {
-    const id = req.params.id
-
-    const userReturn = await User.deleteOne({_id: id});
-
-    if (userReturn.deletedCount === 1) {
-        return res.json({message:"success"});
-    } else {
-        return res.json({message:"failure"});
-    }
-}
-
-const login = async (req, res) => {
-    const usuario = await User.findOne({email:req.body.email});
-    
-    //usuario invalido
-    if (usuario == null) {
-        return res.status.json({"message":"nao existe"});
-    } 
-    //usuario valido
-    else {
-        //senha correta
-        if (await bcrypt.compare(req.body.pass, usuario.pass)) {
-            const id =usuario.id;
-            const token = jwt.sign({ id }, process.env.SECRET, {
-                expiresIn: 300
-            });
-            return res.json({ auth: true, token:token });
-        } 
-        //senha incorreta
-        else {
-            return res.status.json({"message":"senha incorreta"})
-        }
-    }
-}
-
-const validate = (name, email, enroll, pass) => {
-    if (enroll == undefined || pass == undefined || !validateEmail(email) || !validateName(name)) {
-        return false;
-    }
-    return true;
-}
-
+const User = require('../Models/UserSchema');
 
 const validateEmail = (email) => {
-    const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regex.test(email);   
-}
+  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return (regex.test(email) && email !== undefined);
+};
 
 const validateName = (name) => {
-    const regex = /^[a-zA-Z ]{2,30}$/;
-    return regex.test(name);
-} 
+  const regex = /^[a-zA-Z ]{2,30}$/;
+  return (regex.test(name) && name !== undefined);
+};
+
+const validate = (name, email, enroll, pass) => {
+  if (!validateName(name) || !validateEmail(email) || pass === undefined
+    || enroll === undefined || pass.length < 6 || enroll.length === 0) {
+    return false;
+  }
+  return true;
+};
 
 const hashPass = async (pass, saltRounds = 10) => {
-    const salt = await bcrypt.genSalt(saltRounds);
-    return await bcrypt.hash(pass, salt);
-}
+  const salt = await bcrypt.genSalt(saltRounds);
+  return bcrypt.hash(pass, salt);
+};
 
-module.exports = {signUpGet, signUpPost, signUpPut, signUpDelete, login}
+// ROTAS
+const signUpGet = async (req, res) => {
+  const users = await User.find();
+
+  return res.status(200).json(users);
+};
+
+const signUpPost = async (req, res) => {
+  const {
+    name, email, enroll, pass,
+  } = req.body;
+
+  if (!validate(name, email, enroll, pass)) {
+    return res.json({ message: 'invalid' });
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    enroll,
+    pass: await hashPass(pass),
+  });
+
+  return res.json(user);
+};
+
+const signUpPut = async (req, res) => {
+  const { id } = req.params;
+  const {
+    name, email, enroll, pass,
+  } = req.body;
+  let newPass;
+  if (!validate(name, email, enroll, pass)) {
+    return res.json({ message: 'invalid' });
+  }
+
+  const usuarioEncontrado = await User.findOne({ _id: id });
+
+  // Senha nao se altera
+  if (await bcrypt.compare(req.body.pass, usuarioEncontrado.pass)) {
+    newPass = usuarioEncontrado.pass;
+  } else {
+    newPass = await hashPass(pass);
+  }
+
+  const updateReturn = await User.findOneAndUpdate({ _id: id }, {
+    name, email, enroll, pass: newPass,
+  },
+  { new: true }, (err, user) => {
+    if (err) {
+      return res.json(err);
+    }
+    return res.json(user);
+  });
+
+  return updateReturn;
+};
+
+const signUpDelete = async (req, res) => {
+  const { id } = req.params;
+
+  const userReturn = await User.deleteOne({ _id: id });
+
+  if (userReturn.deletedCount === 1) {
+    return res.json({ message: 'success' });
+  }
+  return res.json({ message: 'failure' });
+};
+
+module.exports = {
+  signUpGet, signUpPost, signUpPut, signUpDelete,
+};
