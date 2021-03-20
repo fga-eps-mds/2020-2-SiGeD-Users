28f1318ef8d0e09c159683a21878fa2c8a2ebebc
lucas <lucascandradex14@gmail.com>
2021-03-20 16:32:39 -0300

    [117] change error message from portuguese to english
    Co-authored-by: yukioz <yukio.link@gmail.com>

--- src/Utils/functionsJWT.js
+++ src/Utils/functionsJWT.js
@@ -4,12 +4,12 @@ function verifyJWT(req, res, next) {
   const token = req.headers['x-access-token'];
 
   if (!token) {
-    return res.status(401).json({ auth: false, message: 'Nenhum token fornecido.' });
+    return res.status(401).json({ auth: false, message: 'No token was provided' });
   }
 
   return jwt.verify(token, process.env.SECRET, (err, decoded) => {
     if (err) {
-      return res.status(500).json({ auth: false, message: 'Não foi possível autenticar o token.' });
+      return res.status(500).json({ auth: false, message: 'It was not possible to authenticate the token.' });
     }
     req.userId = decoded.id;
     next();
