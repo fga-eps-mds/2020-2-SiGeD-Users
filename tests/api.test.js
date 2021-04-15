bedacf3e48742b3ca4258af0ae335749ee9c00dd
nYCSTs <lucascandradex14@gmail.com>
2021-04-15 16:58:11 -0300

    [182] add temporary tests
    Co-authored-by: Davi Marinho <davii_marinho@hotmail.com>

---
+++ tests/api.test.js
@@ -0,0 +1,10 @@
+describe('Sample Test', () => {
+    it('Temporary test', async (done) => {
+      expect(true).toBe(true);
+      done();
+    });
+  });
+  afterAll(async (done) => {
+    done();
+  });
+  
